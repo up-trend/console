02babbfbf0bdd7cb608de8d64fe3811e761927d8
Deniz Akşimşek <d.aksimsek1@outlook.com>
2019-11-24 18:55:26 +0300

    kdkdkd

--- js/client-store.js
+++ js/client-store.js
@@ -3,7 +3,11 @@ export default function newClientStore() {
   let clients = {}
   let req = m.request({
     url:"testdata/clients.json",
-  }).then(data => console.log(JSON.stringify(clients = data), m.redraw()))
+    background: true
+  }).then(data => {
+    clients = data
+    m.redraw()
+  })
   return {
     get clients() {
         return clients
